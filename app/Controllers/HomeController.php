d099d9eeaf5f6082cd4515f03aa603fb9911b502
iansummerlin <isummerlin95@gmail.com>
2019-09-22 21:54:02 +0100

    Created User model

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,15 +2,13 @@
 
 namespace App\Controllers;
 
+use App\Models\User;
 use Slim\Views\Twig as View;
 
 class HomeController extends Controller
 {
     public function index($request, $response)
     {
-        $user = $this->db->table('users')->find(1);
-        var_dump($user->email);  
-        die();
         return $this->view->render($response, 'home.twig');
     }
 }
