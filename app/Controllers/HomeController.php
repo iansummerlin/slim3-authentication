0dc3c91ad81636ac0ec3a0ebae4ddc589d0e1fc5
iansummerlin <isummerlin95@gmail.com>
2019-09-22 21:43:02 +0100

    Setting up eloquent

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,6 +8,9 @@ class HomeController extends Controller
 {
     public function index($request, $response)
     {
+        $user = $this->db->table('users')->find(1);
+        var_dump($user->email);  
+        die();
         return $this->view->render($response, 'home.twig');
     }
 }
