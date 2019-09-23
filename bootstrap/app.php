319186213b07ea8f3c73cbe0858479954261eb1f
iansummerlin <isummerlin95@gmail.com>
2019-09-23 15:46:21 +0100

    Persisting old signup form data

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -57,6 +57,7 @@ $container['AuthController'] = function ($container) {
 };
 
 $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
+$app->add(new \App\Middleware\OldInputMiddleware($container));
 
 require __DIR__ . '/../app/routes.php';
 
