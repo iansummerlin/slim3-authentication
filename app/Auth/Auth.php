05c667c641ea503dd0a76a6d860ed4bf68b80d5d
iansummerlin <isummerlin95@gmail.com>
2019-09-24 13:41:15 +0100

    Fixed Auth and ValidationErrorsMiddleware error when session empty and updated navigation bar links

--- app/Auth/Auth.php
+++ app/Auth/Auth.php
@@ -6,6 +6,19 @@ use App\Models\User;
 
 class Auth 
 {
+    public function user()
+    {
+        if (isset($_SESSION['user'])) {
+            return User::find($_SESSION['user']);
+        }
+        return false;
+    }
+
+    public function check()
+    {
+        return isset($_SESSION['user']);
+    }
+
     public function attempt($username, $password)
     {
         $user = User::where('username', $username)->first();
--- app/Middleware/ValidationErrorsMiddleware.php
+++ app/Middleware/ValidationErrorsMiddleware.php
@@ -6,8 +6,9 @@ class ValidationErrorsMiddleware extends Middleware
 {
     public function __invoke($request, $response, $next)
     {
-        $this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
-
+        if(isset($_SESSION['errors'])) {
+            $this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+        }
         $response = $next($request, $response);
         return $response;
     }
