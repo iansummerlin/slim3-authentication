e446d998517cc12ff94a6065c1bf4018f146824d
iansummerlin <isummerlin95@gmail.com>
2019-09-24 06:19:48 +0100

    Set up csrf protection middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -58,8 +58,13 @@ $container['AuthController'] = function ($container) {
     return new \App\Controllers\Auth\AuthController($container);
 };
 
+$container['csrf'] = function ($container) {
+    return new \Slim\Csrf\Guard;
+};
+
 $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
 $app->add(new \App\Middleware\OldInputMiddleware($container));
+$app->add(new \App\Middleware\CsrfViewMiddleware($container));
 
 v::with('App\\Validation\\Rules\\');
 
